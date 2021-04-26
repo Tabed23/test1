88e6c55b113909baf80af67c8aa40295491c9b6f
Tabeed <45124032+Tabed23@users.noreply.github.com>
2021-04-27 00:56:48 +0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
-public class Hello{
-  public static void main(String[] args){
-    System.out.println("Hello, Jenkins Test")
-  }
+public class Hello {
+        public static void main(String[] args){
+                System.out.println("hello, Jenkins Test");
+        }
 }
