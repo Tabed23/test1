6a06cd316c891567352b94e5578a2f824c2bd19e
Tabeed <45124032+Tabed23@users.noreply.github.com>
2021-04-28 20:03:47 +0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
         public static void main(String[] args){
                 System.out.println("hello, Jenkins Test");
+                System.out.println("Web-Hook, Jenkins");
         }
 }
